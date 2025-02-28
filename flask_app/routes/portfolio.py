0cf4a60e3f48d511ec891b8df50622eb9a097bd7
Chris <chrisgig23@users.noreply.github.com>
2025-02-27 21:52:32 -0500

    Remove Debugging code - Issue fixed

--- flask_app/routes/portfolio.py
+++ flask_app/routes/portfolio.py
@@ -15,26 +15,28 @@ def make_purchase(account_id):
     """Handles purchasing stocks based on user allocation."""
     account = Account.query.filter_by(id=account_id, user_id=current_user.id).first_or_404()
 
-    allocations = db.session.query(Allocation.name, Allocation.target) \
-        .filter(Allocation.account_id == account_id).all()
+    # Debugging
+    # allocations = db.session.query(Allocation.name, Allocation.target) \
+    #     .filter(Allocation.account_id == account_id).all()
 
-    print("\nAllocations from Flask query:")
-    for allocation in allocations:
-        print(f"Stock: {allocation.name}, Target: {allocation.target}")
+    # print("\nAllocations from Flask query:")
+    # for allocation in allocations:
+    #     print(f"Stock: {allocation.name}, Target: {allocation.target}")
 
-    included_stocks = db.session.query(Stock.ticker, Stock.isincluded) \
-        .filter(Stock.account_id == account_id).all()
+    # included_stocks = db.session.query(Stock.ticker, Stock.isincluded) \
+    #     .filter(Stock.account_id == account_id).all()
 
-    print("\nStocks and isIncluded status from Flask query:")
-    for stock in included_stocks:
-        print(f"Stock: {stock.ticker}, isIncluded: {stock.isincluded}")
+    # print("\nStocks and isIncluded status from Flask query:")
+    # for stock in included_stocks:
+    #     print(f"Stock: {stock.ticker}, isIncluded: {stock.isincluded}")
 
     total_target_allocation = db.session.query(db.func.sum(db.cast(Allocation.target, db.Float))) \
         .join(Stock, (Stock.ticker == Allocation.name) & (Stock.account_id == Allocation.account_id)) \
         .filter(Stock.account_id == account_id, Stock.isincluded == True) \
         .scalar() or 0
 
-    print(f"\nComputed Total Target Allocation: {total_target_allocation}")
+    # Debugging
+    # print(f"\nComputed Total Target Allocation: {total_target_allocation}")
     if total_target_allocation != 100:
         flash("Please set desired allocation before making a purchase.", 'warning')
         return redirect(url_for('portfolio.adjust_allocation', account_id=account_id))
